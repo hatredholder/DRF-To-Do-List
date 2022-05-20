0f8cb856bb3431d4fa48d4bf8d0922a253e9b2e7
hatredholder <the.great.justice@bk.ru>
2022-05-20 19:40:40 +0300

    reformatted code

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Added Apps
+    # Apps
     'rest_framework',
     'api',
     'frontend'
@@ -122,6 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
